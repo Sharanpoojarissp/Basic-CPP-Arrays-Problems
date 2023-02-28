3a2844d8e883a59cee74fffa62b79d7168451db2
Sharan S poojari <117771260+Sharanpoojarissp@users.noreply.github.com>
2023-02-28 14:16:53 +0530

    Add files via upload

---
+++ 13AInsertBeginning/L13A.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include <bits/stdc++.h>
+#include <cmath>
+using namespace std;
+
+int insertBegining(int arr[] , int n , int insert_this_num){
+    for(int i=n-1 ; i>=0 ; i--){
+        // for(int i = 0 ; i<n ; i++){
+        arr[i+1] = arr[i];
+    }
+        arr[0] = insert_this_num;
+    }
+
+int main(){
+    int arr[10] = {1,2,3,4,5,6,7,8,9};
+    int n = 9;
+    int insert_this_number = 100;
+
+    cout<<"before inserting the 100 in the begining the array was : "<<endl;
+    for(int i=0 ; i<n ; i++){
+        cout<<arr[i]<<" ";
+    }
+
+    cout<<endl;
+
+    insertBegining(arr , n , insert_this_number);
+    cout<<"after inserting the 100 in the begining the array is : "<<endl;
+    for(int i=0 ; i<n ; i++){
+        cout<<arr[i]<<" ";
+    }
+
+     return 0;
+}
